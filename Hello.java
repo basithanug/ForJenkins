f59a8d4c31682ace9e8db5be454f918974ac3c92
afdhal_b_anugrah <afdhal_b_anugrah@telkomsel.co.id>
2020-08-13 17:13:44 +0700

    change to the horde

--- Hello.java
+++ Hello.java
@@ -3,10 +3,7 @@ public class Hello {
   public static void main(String[] args)
   {
     int i=0;
-    while (i<=5) {
-    System.out.println("Hello Ruff World... " + i);
-    i++;
-    }
+    System.out.println("Hello Ruff World... " + " and the power of HORDE");
   }
 
 
